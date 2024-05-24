afbeff31bf9715ccdb693aa0f8dc01d04918cffb
Alex Avila <alex.ramon2004@gmail.com>
2024-05-24 08:56:23 -0500

    chore: project configuration.

---
+++ src/main/java/com/github/inncontrol/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,27 @@
+package com.github.inncontrol.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi.info(new Info()
+                        .title("Inn Control Platform API")
+                        .description("Inn Control application REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Inn Control platform wiki documentation")
+                        .url("https://inncontrol-platform.wiki.github.io/docs"));
+        return openApi;
+    }
+}
