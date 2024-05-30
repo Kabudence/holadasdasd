eb230e328c7e443e8996891c7afb69a70198d67d
Alex Avila <alex.ramon2004@gmail.com>
2024-05-30 17:46:14 -0500

    feat: created aggregates, command, query  and interceptors.

---
+++ src/main/java/com/github/inncontrol/communications/domain/services/MessageQueryService.java
@@ -0,0 +1,9 @@
+package com.github.inncontrol.communications.domain.services;
+
+/**
+ * Created by Alex Avila Asto - A.K.A (Ryzeon)
+ * Project: inncontrol-backend
+ * Date: 5/29/24 @ 12:39
+ */
+public interface MessageQueryService {
+}
