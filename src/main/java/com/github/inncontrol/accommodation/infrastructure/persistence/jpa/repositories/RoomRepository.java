8aaf78f09fa6d862bbbb50d31cc8ae9cd30ada92
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-05-31 18:59:11 -0500

    feat(Accommodation-repository): added Room Repository

--- src/main/java/com/github/inncontrol/accommodation/infrastructure/persistence/jpa/repositories/RoomRepository.java
+++ src/main/java/com/github/inncontrol/accommodation/infrastructure/persistence/jpa/repositories/RoomRepository.java
@@ -1,6 +1,7 @@
 package com.github.inncontrol.accommodation.infrastructure.persistence.jpa.repositories;
 
 import com.github.inncontrol.accommodation.domain.model.aggregates.Room;
+import com.github.inncontrol.accommodation.domain.model.valueobjects.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    Optional<Room> findByRoomType(String roomType);
-    boolean existsByRoomType(String roomType);
+    Optional<Room> findByRoomType(RoomType roomType);
+    boolean existsByRoomType(RoomType roomType);
 }
