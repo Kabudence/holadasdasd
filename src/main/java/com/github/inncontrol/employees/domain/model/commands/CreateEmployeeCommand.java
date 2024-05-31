c16d6ba2daf837e793d0320160b6854a22128290
USER <u202119880@upc.udu.pe>
2024-05-31 00:39:42 -0500

    feat:add new parameters

--- src/main/java/com/github/inncontrol/employees/domain/model/commands/CreateEmployeeCommand.java
+++ src/main/java/com/github/inncontrol/employees/domain/model/commands/CreateEmployeeCommand.java
@@ -1,4 +1,4 @@
 package com.github.inncontrol.employees.domain.model.commands;
 
-public record CreateEmployeeCommand() {
+public record CreateEmployeeCommand(String firstName, String lastName, String email, String street, String number, String city, String postalCode, String country) {
 }
