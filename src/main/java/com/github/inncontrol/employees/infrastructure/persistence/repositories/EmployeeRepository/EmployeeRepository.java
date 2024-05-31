7d6fc96566d40ab1a55a6e829e7b4297d7648b73
USER <u202119880@upc.udu.pe>
2024-05-30 22:42:26 -0500

    feat:add implementation of Domain Layer of bounded context(profile) in aggregate root about Profile business logic

--- src/main/java/com/github/inncontrol/employees/infrastructure/persistence/repositories/EmployeeRepository/EmployeeRepository.java
+++ src/main/java/com/github/inncontrol/employees/infrastructure/persistence/repositories/EmployeeRepository/EmployeeRepository.java
@@ -1,9 +1,19 @@
 package com.github.inncontrol.employees.infrastructure.persistence.repositories.EmployeeRepository;
 
+import com.github.inncontrol.employees.domain.model.aggregates.Employee;
+import com.github.inncontrol.employees.domain.model.valueobjects.ProfileId;
 import com.github.inncontrol.employees.domain.model.valueobjects.RoleStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository  extends JpaRepository<Employee,Long> {
     boolean existsByRole(RoleStatus role);
+    boolean findByProfileId(ProfileId profileId);
+    List<Employee>findByByInitiationDate(Date initializationDate);
+    Optional<Employee>findByRoleStatus(RoleStatus role);
 }
