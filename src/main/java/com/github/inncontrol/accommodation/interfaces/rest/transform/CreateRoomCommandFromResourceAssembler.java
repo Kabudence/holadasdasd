542d1f4ebf61c4ba4cf02a75887f37aab07144c0
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-05-31 19:00:36 -0500

    feat(Accommodation-Room-Assemblers): added Room Assemblers

---
+++ src/main/java/com/github/inncontrol/accommodation/interfaces/rest/transform/CreateRoomCommandFromResourceAssembler.java
@@ -0,0 +1,10 @@
+package com.github.inncontrol.accommodation.interfaces.rest.transform;
+
+import com.github.inncontrol.accommodation.domain.model.commands.CreateRoomCommand;
+import com.github.inncontrol.accommodation.interfaces.rest.resources.CreateRoomResource;
+
+public class CreateRoomCommandFromResourceAssembler {
+    public static CreateRoomCommand toCommandFromResource(CreateRoomResource resource){
+        return new CreateRoomCommand(resource.firstName(), resource.lastName(), resource.type(), resource.state());
+    }
+}
