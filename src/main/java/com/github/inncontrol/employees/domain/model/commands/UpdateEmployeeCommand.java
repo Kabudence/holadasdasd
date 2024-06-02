06fc43abb31cc8ec12a03c0989ec930764421e3c
USER <u202119880@upc.udu.pe>
2024-06-01 23:20:37 -0500

    feat:add new implementation on EmployeeCommandService that handles the writing of the bounded context employee

--- src/main/java/com/github/inncontrol/employees/domain/model/commands/UpdateEmployeeCommand.java
+++ src/main/java/com/github/inncontrol/employees/domain/model/commands/UpdateEmployeeCommand.java
@@ -1,4 +1,8 @@
 package com.github.inncontrol.employees.domain.model.commands;
 
-public record UpdateEmployeeCommand() {
+import com.github.inncontrol.employees.domain.model.valueobjects.ContractInformation;
+import com.github.inncontrol.employees.domain.model.valueobjects.Role;
+
+public record UpdateEmployeeCommand(Long id, String email, Double salary,
+                                    ContractInformation contractInformation, Role role) {
 }
