df597e3563d5068d3aec027eb7ba0b9b2da988e1
USER <u202119880@upc.udu.pe>
2024-06-01 23:22:06 -0500

    feat:add new implementation on transform and resource that manages the information entering and exiting the interface layer   of the bounded context employee

---
+++ src/main/java/com/github/inncontrol/employees/interfaces/rest/resources/UpdateEmployeeResource.java
@@ -0,0 +1,11 @@
+package com.github.inncontrol.employees.interfaces.rest.resources;
+
+import com.github.inncontrol.employees.domain.model.valueobjects.ContractInformation;
+import com.github.inncontrol.employees.domain.model.valueobjects.Role;
+
+public record UpdateEmployeeResource(
+                                     String email,
+                                     Double salary,
+                                     ContractInformationResource contractinformationResource,
+                                     Role role) {
+}
