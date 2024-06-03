1f70cb17136d599051c44f9c6f3fb138b6fabdd9
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-06-02 19:31:29 -0500

    feat(Accommodation-Room): added date initialDate and finalDate, roomNumber and update information

--- src/main/java/com/github/inncontrol/accommodation/domain/model/aggregates/Room.java
+++ src/main/java/com/github/inncontrol/accommodation/domain/model/aggregates/Room.java
@@ -2,6 +2,7 @@ package com.github.inncontrol.accommodation.domain.model.aggregates;
 
 import com.github.inncontrol.accommodation.domain.model.commands.CreateRoomCommand;
 import com.github.inncontrol.accommodation.domain.model.valueobjects.GuestName;
+import com.github.inncontrol.accommodation.domain.model.valueobjects.RoomReservation;
 import com.github.inncontrol.accommodation.domain.model.valueobjects.RoomStatus;
 import com.github.inncontrol.accommodation.domain.model.valueobjects.RoomType;
 import com.github.inncontrol.shared.domain.aggregates.AuditableAbstractAggregateRoot;
@@ -9,9 +10,16 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Getter;
+
+import java.util.Date;
 
 @Entity
 public class Room extends AuditableAbstractAggregateRoot<Room> {
+
+    @Getter
+    private int roomNumber;
+
     @Embedded
     private GuestName guest;
 
@@ -21,19 +29,35 @@ public class Room extends AuditableAbstractAggregateRoot<Room> {
     @Enumerated(EnumType.STRING)
     private RoomStatus status;
 
-    public Room(String firstName, String lastName) {
+    @Embedded
+    private RoomReservation reservation;
+
+    public Room(String firstName, String lastName, String roomType, String roomStatus, int roomNumber, Date initialDate, Date finalDate) {
         this.guest = new GuestName(firstName, lastName);
-        this.type = RoomType.Standard;
-        this.status = RoomStatus.Vacant;
+        this.roomNumber = roomNumber;
+        this.reservation = new RoomReservation(initialDate,finalDate);
+        this.type = RoomType.valueOf(roomType);
+        this.status = RoomStatus.valueOf(roomStatus);
     }
 
     public Room(CreateRoomCommand command) {
         this.guest = new GuestName(command.firstName(), command.lastName());
         this.type = RoomType.valueOf(command.type());
         this.status = RoomStatus.valueOf(command.Status());
+        this.roomNumber = command.roomNumber();
+        this.reservation = new RoomReservation(command.initialDate(), command.finalDate());
     }
 
     public Room() {
+    }
+
+    public Room updateInformation(String firstName, String lastName,String roomType, String roomStatus, int roomNumber, Date initialDate, Date finalDate){
+        this.guest = new GuestName(firstName, lastName);
+        this.roomNumber = roomNumber;
+        this.type = RoomType.valueOf(roomType);
+        this.status = RoomStatus.valueOf(roomStatus);
+        this.reservation = new RoomReservation(initialDate,finalDate);
+        return this;
     }
 
     public void updateGuestName(String firstName, String lastName) {
@@ -51,4 +75,7 @@ public class Room extends AuditableAbstractAggregateRoot<Room> {
     public String getStatus() {
         return this.status.name().toLowerCase();
     }
+
+    public String getReservationDate() {return reservation.getFullReservationDate();}
+
 }
