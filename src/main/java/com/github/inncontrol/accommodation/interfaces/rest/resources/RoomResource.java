a5476df0461d089b361f55f62f1dca1329fa9d34
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-06-02 19:30:08 -0500

    feat(Accommodation-Room-Resources): added room Number, reservation

--- src/main/java/com/github/inncontrol/accommodation/interfaces/rest/resources/RoomResource.java
+++ src/main/java/com/github/inncontrol/accommodation/interfaces/rest/resources/RoomResource.java
@@ -4,6 +4,8 @@ public record RoomResource(
         Long id,
         String fullName,
         String roomType,
-        String roomStatus
+        String roomStatus,
+        int roomNumber,
+        String roomReservation
 ) {
 }
