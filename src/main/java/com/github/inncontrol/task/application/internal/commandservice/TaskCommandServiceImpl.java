64f554b02c5a5d41c5d2b0beded558640ebd4317
PieroPeriche <164218254+PieroPeriche@users.noreply.github.com>
2024-06-06 05:17:31 +0000

    feat(task): implemented services

--- src/main/java/com/github/inncontrol/task/application/internal/commandservice/TaskCommandServiceImpl.java
+++ src/main/java/com/github/inncontrol/task/application/internal/commandservice/TaskCommandServiceImpl.java
@@ -1,14 +1,39 @@
 package com.github.inncontrol.task.application.internal.commandservice;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.github.inncontrol.task.domain.model.aggregates.Task;
+import com.github.inncontrol.task.domain.model.commands.CompleteTaskCommand;
+import com.github.inncontrol.task.domain.model.commands.CreateTaskCommand;
+import com.github.inncontrol.task.domain.model.commands.StartTaskCommand;
+import com.github.inncontrol.task.domain.services.TaskCommandService;
 import com.github.inncontrol.task.infrastructure.persistence.jpa.TaskRepository;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class TaskCommandServiceImpl {
+public class TaskCommandServiceImpl implements TaskCommandService {
 
     private final TaskRepository taskRepository;
+
+    @Override
+    public Optional<Task> handle(CreateTaskCommand command) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public void handle(StartTaskCommand command) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public void handle(CompleteTaskCommand command) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
 }
--- src/main/java/com/github/inncontrol/task/application/internal/queryservice/TaskQueryServiceImpl.java
+++ src/main/java/com/github/inncontrol/task/application/internal/queryservice/TaskQueryServiceImpl.java
@@ -1,15 +1,55 @@
 package com.github.inncontrol.task.application.internal.queryservice;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.github.inncontrol.task.domain.model.aggregates.Task;
+import com.github.inncontrol.task.domain.model.queries.GetAllTaskForEmployeeQuery;
+import com.github.inncontrol.task.domain.model.queries.GetAllTaskFromDatesForEmployeeQuery;
+import com.github.inncontrol.task.domain.model.queries.GetAllTaskInWeekForEmployeeQuery;
+import com.github.inncontrol.task.domain.model.queries.GetAllTaskQuery;
+import com.github.inncontrol.task.domain.model.queries.GetTaskByIdQuery;
+import com.github.inncontrol.task.domain.services.TaskQueryService;
 import com.github.inncontrol.task.infrastructure.persistence.jpa.TaskRepository;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class TaskQueryServiceImpl {
+public class TaskQueryServiceImpl implements TaskQueryService {
 
     private final TaskRepository taskRepository;
+
+    @Override
+    public Optional<Task> handle(GetTaskByIdQuery query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public List<Task> handle(GetAllTaskQuery query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public List<Task> handle(GetAllTaskForEmployeeQuery query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public List<Task> handle(GetAllTaskInWeekForEmployeeQuery query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
+
+    @Override
+    public List<Task> handle(GetAllTaskFromDatesForEmployeeQuery query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
 
 }
