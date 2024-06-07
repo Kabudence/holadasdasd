513513c0613ee96bf3039aad87113fec92851919
Erick <u202119880@upc.udu.pe>
2024-06-07 12:07:21 -0500

    feat(IAM):Add implementation about IAM BOUNDED CONTEXT

--- src/main/java/com/github/inncontrol/iam/domain/model/queries/GetRoleByNameQuery.java
+++ src/main/java/com/github/inncontrol/iam/domain/model/queries/GetRoleByNameQuery.java
@@ -3,5 +3,5 @@ package com.github.inncontrol.iam.domain.model.queries;
 
 import com.github.inncontrol.iam.domain.model.valueobjects.Roles;
 
-public record GetRoleByNameQuery(Roles roleName) {
+public record GetRoleByNameQuery (Roles roleName) {
 }
--- src/main/java/com/github/inncontrol/iam/infrastructure/tokens/jwt/BearerTokenService.java
+++ src/main/java/com/github/inncontrol/iam/infrastructure/tokens/jwt/BearerTokenService.java
@@ -4,7 +4,7 @@ import com.github.inncontrol.iam.application.internal.outboundservices.tokens.To
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 
-public interface BearerTokenService extends TokenService {
+public interface BearerTokenService  extends TokenService {
 
     String getBearerTokenFrom(HttpServletRequest request);
     String generateToken(Authentication authentication);
