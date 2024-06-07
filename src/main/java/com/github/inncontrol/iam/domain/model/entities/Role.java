aa2cb79364684bd6967ff868e8fc4099f417321c
Erick <u202119880@upc.udu.pe>
2024-06-07 12:06:56 -0500

    feat(IAM):Add implementation about IAM BOUNDED CONTEXT

--- src/main/java/com/github/inncontrol/iam/domain/model/entities/Role.java
+++ src/main/java/com/github/inncontrol/iam/domain/model/entities/Role.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Role {
     @Getter
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy  = GenerationType.IDENTITY)
     private Long id;
 
     @Getter
