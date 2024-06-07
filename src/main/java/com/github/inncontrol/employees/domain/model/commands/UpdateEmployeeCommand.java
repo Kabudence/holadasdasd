ca958f7b7947c3984bbbaa7686c85cd46e2f9d1e
Erick <U201910151@upc.edu.pe>
2024-06-07 12:14:33 -0500

    feat(IAM):Add implementation about IAM BOUNDED CONTEXT

--- src/main/java/com/github/inncontrol/employees/domain/model/commands/UpdateEmployeeCommand.java
+++ src/main/java/com/github/inncontrol/employees/domain/model/commands/UpdateEmployeeCommand.java
@@ -3,6 +3,6 @@ package com.github.inncontrol.employees.domain.model.commands;
 import com.github.inncontrol.employees.domain.model.valueobjects.ContractInformation;
 import com.github.inncontrol.employees.domain.model.valueobjects.Role;
 
-public record UpdateEmployeeCommand(Long id, String email, Double salary,
+public record  UpdateEmployeeCommand(Long id, String email, Double salary,
                                     ContractInformation contractInformation, Role role) {
 }
