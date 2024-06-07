ead59e6e7955f19f34e841265dd89e23e974a350
Erick <u202119880@upc.udu.pe>
2024-06-07 12:07:17 -0500

    feat(IAM):Add implementation about IAM BOUNDED CONTEXT

--- src/main/java/com/github/inncontrol/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
+++ src/main/java/com/github/inncontrol/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
@@ -3,6 +3,6 @@ package com.github.inncontrol.iam.infrastructure.hashing.bcrypt;
 import com.github.inncontrol.iam.application.internal.outboundservices.hashing.HashingService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public interface BCryptHashingService extends HashingService, PasswordEncoder {
+public interface BCryptHashingService  extends HashingService, PasswordEncoder {
 
 }
