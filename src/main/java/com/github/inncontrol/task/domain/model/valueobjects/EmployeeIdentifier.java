cffb76327cb92d9db73b29d1dec60335aebfd625
Ryzeon <alex.ramon2004@gmail.com>
2024-06-07 00:20:15 -0500

    fix(employee): fix id conflicts.

--- src/main/java/com/github/inncontrol/task/domain/model/valueobjects/EmployeeIdentifier.java
+++ src/main/java/com/github/inncontrol/task/domain/model/valueobjects/EmployeeIdentifier.java
@@ -3,5 +3,5 @@ package com.github.inncontrol.task.domain.model.valueobjects;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public record EmployeeIdentifier(Long id){
+public record EmployeeIdentifier(Long employeeId) {
 }
