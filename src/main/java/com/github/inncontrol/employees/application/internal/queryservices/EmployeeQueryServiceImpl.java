51bc99b594d259cfec3d7e769fc2b962a7a372ae
Erick <U201910151@upc.edu.pe>
2024-06-07 12:14:28 -0500

    feat(IAM):Add implementation about IAM BOUNDED CONTEXT

--- src/main/java/com/github/inncontrol/employees/application/internal/queryservices/EmployeeQueryServiceImpl.java
+++ src/main/java/com/github/inncontrol/employees/application/internal/queryservices/EmployeeQueryServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class EmployeeQueryServiceImpl implements EmployeeQueryService {
+public class  EmployeeQueryServiceImpl implements EmployeeQueryService {
 
     private final EmployeeRepository employeeRepository;
 
