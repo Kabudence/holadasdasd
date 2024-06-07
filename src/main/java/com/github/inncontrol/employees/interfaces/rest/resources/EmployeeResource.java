1f245fa38863055c57369bd708905068b466af30
Erick <U201910151@upc.edu.pe>
2024-06-07 12:21:04 -0500

    feat(IAM):Add implementation about IAM BOUNDED CONTEXT

--- src/main/java/com/github/inncontrol/employees/interfaces/rest/resources/EmployeeResource.java
+++ src/main/java/com/github/inncontrol/employees/interfaces/rest/resources/EmployeeResource.java
@@ -4,7 +4,7 @@ import com.github.inncontrol.employees.domain.model.valueobjects.Role;
 
 import java.util.Date;
 
-public record EmployeeResource(Role role, Double salary,
+public record  EmployeeResource(Role role, Double salary,
                                Date initiationContract,
                                Date terminationContract,
                                Long profileId
