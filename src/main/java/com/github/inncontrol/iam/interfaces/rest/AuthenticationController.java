717f4a25f9da638957485d08c14773f4f8977951
Alex Avila <alex.ramon2004@gmail.com>
2024-06-19 00:55:09 -0500

    feat(iam): added roles, sign up.

--- src/main/java/com/github/inncontrol/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/github/inncontrol/iam/interfaces/rest/AuthenticationController.java
@@ -15,9 +15,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 /**
