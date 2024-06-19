2f75efa8c96228157e2ff06fd89391899711ce62
Alex Avila <alex.ramon2004@gmail.com>
2024-06-19 12:25:21 -0400

    Update OpenApiConfiguration.java

--- src/main/java/com/github/inncontrol/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/github/inncontrol/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
